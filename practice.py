013d13484cc35bcd3de65c53f054525b46630253
Brian Park <3820362@Mobis.HKMG.Global>
2022-01-26 21:51:09 +0000

    half done

--- practice.py
+++ practice.py
@@ -726,33 +726,139 @@
 
 # 전달값과 반환값 () (2:30:10)
 
-def open_account(): # 함수 정의 시에는 "def + 함수 이름() + :"
-    print("새로운 계좌가 생성 되었습니다.")
+# def open_account(): # 함수 정의 시에는 "def + 함수 이름() + :"
+#     print("새로운 계좌가 생성 되었습니다.")
 
-def deposit(balance, money): # 입금
-    print("입금이 완료 되었습니다. 잔액은 {0} 원 입니다.".format(balance + money))
-    return balance + money
+# def deposit(balance, money): # 입금
+#     print("입금이 완료 되었습니다. 잔액은 {0} 원 입니다.".format(balance + money))
+#     return balance + money
 
-def withdraw(balance, money): # 출금
-    if balance >= money: # 잔액이 출금보다 많으면
-        print("출금이 완료되었습니다. 잔액은 {0} 원 입니다.".format(balance - money))
-        return balance - money
+# def withdraw(balance, money): # 출금
+#     if balance >= money: # 잔액이 출금보다 많으면
+#         print("출금이 완료되었습니다. 잔액은 {0} 원 입니다.".format(balance - money))
+#         return balance - money
+#     else:
+#         print("출금이 완료되지 않았습니다. 잔액은 {0} 원 입니다.".format(balance))
+#         return balance
+
+# def withdraw_night(balance, money): #저녁에 출금
+#     commission = 100 # 수수료 100원
+#     return commission, balance - money - commission # 여러개의 값을 튜플 형식으로 반환
+
+# balance = 0 # 잔액
+# balance = deposit(balance, 1000)
+# # balance = withdraw(balance, 2000)
+# # balance = withdraw(balance, 500)
+# commission, balance = withdraw_night(balance, 500)
+# print("수수료 {0} 원이며, 잔액은 {1} 원 입니다.".format(commission, balance))
+
+# 기본값 () (2:37:50) # 기본값을 설정하는 방법
+
+# def profile(name, age, main_lang):
+#     print("이름: {0}\t나이: {1}\t주 사용 언어: {2}"\
+#         .format(name, age, main_lang)) # 역 슬래쉬(\)를 사용하면 두줄의 내용을 한줄로 이어 준다.
+
+# profile("유재석", 20, "파이썬")
+# profile("김태호", 25, "자바")
+
+# # 같은 학교 같은 학년 같은 반 같은 수업
+
+# def profile(name, age=17, main_lang="파이썬"): # 함수가 호출 될때 값을 전달 받지 못하면 기본값을 사용한다.
+#     print("이름: {0}\t나이: {1}\t주 사용 언어: {2}"\
+#         .format(name, age, main_lang)) # 역 슬래쉬(\)를 사용하면 두줄의 내용을 한줄로 이어 준다.
+
+# profile("유재석")
+# profile("김태호")
+
+# 키워드 값 () (2:41:32) 
+
+# def profile(name, age, main_lang):
+#     print(name, age, main_lang)
+
+# #함수에서 전달 받는 매개변수의 값을 키워드를 이용해서 함수를 호출 해 주면, 그 키워드에 해당하는 값이 순서가 뒤섞여 있어도 잘 전달 됨
+# profile(name="유재석", main_lang="파이썬", age=20)
+# profile(main_lang="자바", age=25, name="김태호")
+
+# 가변인자 (7-5) (2:43:05)
+
+# # def profile(name, age, lang1, lang2, lang3, lang4, lang5):
+# #     print("이름: {0} \t나이: {1} \t".format(name, age), end=" ")
+# #     print(lang1, lang2, lang3, lang4, lang5)
+
+# def profile(name, age, *language):
+#     print("이름: {0} \t나이: {1} \t".format(name, age), end=" ")
+#     for lang in language:
+#         print(lang, end=" ")
+#     print()
+
+# profile("유재석", 20, "Python", "Java","C", "C++", "C#")
+# profile("김태호", 20, "Kotlin", "Swift")
+
+# 지역변수와 전역변수 (7-6) (2:48:00)
+
+# gun = 10
+
+# def checkpoint(Soldiers): #경계근무
+#     global gun # 전역 공간에 있는 gun 사용
+#     gun = gun - Soldiers
+#     print("[함수 내] 남은 총: {0}".format(gun))
+
+# def checkpoint_return(gun, soldiers):
+#     gun = gun - soldiers
+#     print("[함수 내] 남은 총: {0}".format(gun))
+#     return gun
+
+# print("전체 총: {0}".format(gun))
+# gun = checkpoint_return(gun, 2) # 2명이 경계 근무 나감
+# print("남은 총: {0}".format(gun)) # 함수 내에서 전역 변수에 대해 변경이 있으면 함수 이후에도 그 값이 남아 있음
+
+# 퀴즈 #6 (2:53:57)
+
+# Quiz) 표준 체중을 구하는 프로그램을 작성하시오.
+
+# * 표준 체중: 각 개인의 키에 적당한 체중
+
+# (성별에 따른 공식)
+# 남자: 키(m) x 키(m) x 22
+# 여자: 키(m) x 키(m) x 21
+
+# 조건1: 표준 체중은 별도의 함수 내에서 계산
+#  * 함수명: std_weight
+#  * 전달값: 키(height), 성별(gender)
+# 조건2: 표준 체중은 소수점 둘째 자리까지 표시
+
+# (출력 예제)
+# 키 175cm 남자의 표준 체중은 67.38kg 입니다.
+
+def std_weight (height, gender):
+    weight = 0
+    if gender == "남자":
+        weight = height * 0.01 * height * 0.01 * 22
+    elif gender == "여자":
+        weight = height * 0.01 * height * 0.01 * 21
+    return weight
+
+gender = "여자"
+height = 160
+weight = std_weight(height, gender)
+
+print("키 {0}cm {1}의 표준 체중은 {2}kg 입니다.".format(height, gender, weight))
+
+# Answer
+def std_weight (height, gender): # 키 m 단위 (실수), 성별 "남자" / "여자"
+    if gender == "남자":
+        return height * height * 22
     else:
-        print("출금이 완료되지 않았습니다. 잔액은 {0} 원 입니다.".format(balance))
-        return balance
+        return height * height * 21
 
-def withdraw_night(balance, money): #저녁에 출금
-    commission = 100 # 수수료 100원
-    return commission, balance - money - commission # 여러개의 값을 튜플 형식으로 반환
+height = 175 # cm 단위
+gender = "남자"
+weight = round(std_weight(height / 100, gender), 2) # 소수점 2째 자리 까지 표시 
+print("키 {0}cm {1}의 표준 체중은 {2}kg 입니다.".format(height, gender, weight))
 
-balance = 0 # 잔액
-balance = deposit(balance, 1000)
-# balance = withdraw(balance, 2000)
-# balance = withdraw(balance, 500)
-commission, balance = withdraw_night(balance, 500)
-print("수수료 {0} 원이며, 잔액은 {1} 원 입니다.".format(commission, balance))
+# 위의 내 답처럼 weight 이라는 변수를 써서 돌려 받을 때는 소수점 2자리 까지만 나옴, 
+# 아래 youtube의 정답처럼 계산식으로 바로 돌려 받을 경우는 소수점 3자리 까지 나옴 - round로 소수점 자리 표시
 
-# 기본값 () (2:37:50)
-
+# 표준 입출력 (8-1) (2:59:00)
 
 
